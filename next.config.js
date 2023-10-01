870c2ce19db8d197ba45401b5d8bef781f320502
Dan Nguyen <danthedevnguyen@gmail.com>
2023-10-01 11:40:50 -0400

    test deployment - next.config.js edits

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL],
-  },
+  // images: {
+  //   domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL],
+  // },
 }

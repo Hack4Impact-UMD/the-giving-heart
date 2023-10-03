4d2e87ff980ee4a0c38c793509c880cfc34f89ce
Alex Chen <achen131@umd.edu>
2023-10-03 16:15:42 -0400

    replace payload home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,19 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={classes.body}>
         <header className={classes.header}>
-          <Link href="https://payloadcms.com" target="_blank" rel="noopener noreferrer">
-            <picture>
-              <source
-                media="(prefers-color-scheme: dark)"
-                srcSet="https://raw.githubusercontent.com/payloadcms/payload/master/src/admin/assets/images/payload-logo-light.svg"
-              />
-              <img
-                className={classes.logo}
-                alt="Payload Logo"
-                src="https://raw.githubusercontent.com/payloadcms/payload/master/src/admin/assets/images/payload-logo-dark.svg"
-              />
-            </picture>
-          </Link>
+          The Giving Heart
         </header>
         {children}
       </body>

ce98e61a0647fed9d6d4e4718ff117b55598c7df
achou1044 <64855502+achou1044@users.noreply.github.com>
2023-10-14 19:35:35 -0400

    Coded frontend templates for Home, footer, and header components

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -6,6 +6,7 @@ import { fetchFooter, fetchGlobals } from '../../_api/fetchGlobals'
 import { ThemeSelector } from '../../_providers/Theme/ThemeSelector'
 import { Gutter } from '../Gutter'
 import { CMSLink } from '../Link'
+import { Box, Typography, Divider } from '@mui/material/'
 
 import classes from './index.module.scss'
 
@@ -26,31 +27,84 @@ export async function Footer() {
   return (
     <footer className={classes.footer}>
       <Gutter className={classes.wrap}>
-        <Link href="/">
-          <picture>
-            <img
-              className={classes.logo}
-              alt="Payload Logo"
-              src="https://raw.githubusercontent.com/payloadcms/payload/master/src/admin/assets/images/payload-logo-light.svg"
-            />
-          </picture>
-        </Link>
-        <nav className={classes.nav}>
-          <ThemeSelector />
-          {navItems.map(({ link }, i) => {
-            return <CMSLink key={i} {...link} />
-          })}
-          <Link href="/admin">Admin</Link>
-          <Link
-            href="https://github.com/payloadcms/payload/tree/master/templates/website"
-            target="_blank"
-            rel="noopener noreferrer"
+        <nav clasName={classes.nav}>
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
           >
-            Source Code
-          </Link>
-          <Link href="https://payloadcms.com" target="_blank" rel="noopener noreferrer">
-            Payload
-          </Link>
+            <Typography sx={{ fontWeight: 'bold', fontSize: 'h4.fontSize', width: '25%' }}>
+              The Giving
+              <br />
+              Heart
+            </Typography>
+
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: 'bold' }}>Contact Us</Typography>
+              <Typography>Info@givingheart.com</Typography>
+              <Typography>(302)-532-2922</Typography>
+            </Box>
+
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: 'bold' }}>Useful Links</Typography>
+              <Link
+                href="https://github.com/payloadcms/payload/tree/master/templates/website"
+                target="_blank"
+                rel="noopener noreferrer"
+                sx={{ align: 'center' }}
+              >
+                Donate
+              </Link>
+              <Link
+                href="https://github.com/payloadcms/payload/tree/master/templates/website"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Volunteer
+              </Link>
+            </Box>
+
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="space-around"
+              alignItems="center"
+            >
+              <Typography sx={{ fontWeight: 'bold' }}>Socials</Typography>
+              <Box display="flex" flexDirection="row">
+                <img alt="S1" src="" />
+                <img alt="S2" src="" />
+                <img alt="S3" src="" />
+              </Box>
+            </Box>
+          </Box>
+
+          <br />
+          <Divider sx={{ color: '#606060' }} />
+
+          <Box display="flex" justifyContent="space-between" alignItems="center">
+            <Typography sx={{ fontSize: 'body2.fontSize' }}>The Giving Heart</Typography>
+            <Typography sx={{ fontSize: 'body2.fontSize' }}>
+              <Link href="" target="_blank" rel="noopener noreferrer">
+                Privacy Policy
+              </Link>
+              |
+              <Link href="" target="_blank" rel="noopener noreferrer">
+                Terms of Use
+              </Link>
+            </Typography>
+          </Box>
         </nav>
       </Gutter>
     </footer>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,142 @@
+import React from 'react'
+import { Box, Typography, Button } from '@mui/material/'
+import { Metadata } from 'next'
+
+import { Gutter } from './_components/Gutter'
+import { RenderParams } from './_components/RenderParams'
+import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
 import PageTemplate, { generateMetadata } from './(pages)/[slug]/page'
 
-export default PageTemplate
+import classes from './(pages)/home/index.module.scss'
 
-export { generateMetadata }
+export default async function Home() {
+  return (
+    <nav className={classes.nav}>
+      <Box
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="space-around"
+        sx={{
+          width: '100%',
+          height: '200px',
+          color: 'white',
+          backgroundColor: 'darkgrey',
+          margin: '0',
+        }}
+      >
+        <img alt="Background Image" src="" />
+
+        <Typography>Short description of org</Typography>
+      </Box>
+
+      <Gutter>
+        <RenderParams className={classes.params} />
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'white',
+          }}
+        >
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              boxShadow: 5,
+              width: '60%',
+              height: '30%',
+              backgroundColor: '#E8E8E8',
+              padding: '3%',
+            }}
+          >
+            <Typography sx={{ fontWeight: 'bold', fontSize: 'h5.fontSize' }}>Heading</Typography>
+            <Typography>Content</Typography>
+            <br />
+            <Button sx={{ backgroundColor: 'darkgrey', color: 'white' }}> Get Involved </Button>
+          </Box>
+
+          <RenderParams className={classes.params} />
+
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              width: '70%',
+              height: '30%',
+              backgroundColor: 'lightgray',
+              padding: '3%',
+            }}
+          >
+            <Typography sx={{ fontSize: 'h4.fontSize' }}>Our Impact So Far</Typography>
+            <br />
+            <Box
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="space-around"
+              sx={{
+                width: '100%',
+              }}
+            >
+              <Box>
+                <Typography textAlign="center" sx={{ fontSize: 'h5.fontSize' }}>
+                  10,000
+                </Typography>
+                <Typography textAlign="center" sx={{ fontSize: 'body2.fontSize' }}>
+                  Meals Provided
+                </Typography>
+              </Box>
+              <Box>
+                <Typography textAlign="center" sx={{ fontSize: 'h5.fontSize' }}>
+                  1000
+                </Typography>
+                <Typography textAlign="center" sx={{ fontSize: 'body2.fontSize' }}>
+                  Volunteers
+                </Typography>
+              </Box>
+              <Box>
+                <Typography textAlign="center" sx={{ fontSize: 'h5.fontSize' }}>
+                  $100,000
+                </Typography>
+                <Typography textAlign="center" sx={{ fontSize: 'body2.fontSize' }}>
+                  Raised
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+
+          <RenderParams className={classes.params} />
+
+          <Typography textAlign="center" sx={{ fontSize: 'h4.fontSize' }}>
+            Gallery
+          </Typography>
+
+          <Typography>Replace this with the gallery component</Typography>
+
+        </Box>
+      </Gutter>
+    </nav>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Welcome to the Home Page',
+  openGraph: mergeOpenGraph({
+    title: 'Home',
+    url: '/',
+  }),
+}
+
+//export default PageTemplate
+
+//export { generateMetadata }

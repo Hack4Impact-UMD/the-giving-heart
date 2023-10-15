b453ef29008711437f67f0bf083e59aa059cb221
Gelila Kebede <gelilakebede123@gmail.com>
2023-10-15 13:12:45 -0400

    removed left nav bar; minor styling fixes

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,43 +1,74 @@
 import React from 'react'
-import { BrowserRouter } from "react-router-dom";
 
 import { Card } from '../../_components/Card'
-import { Gutter } from '../../_components/Gutter'
 import { CardContainer } from '../../_components/CardContainer'
-
+import { Gutter } from '../../_components/Gutter'
 
 import classes from './index.module.scss'
-import { LeftNavBar } from '../../_components/LeftNavBar';
-
 
 export default async function Dashboard() {
   return (
-    <div className={classes.mainContainer}>
-        <LeftNavBar />
-        
-        {/* <Gutter> */}
-        <div className={classes.container}>
-            <h1> Welcome, [firstName]! </h1>
-            
-            <div className={classes.grayContainer}>
-                Account Type: Volunteer
-            </div>
+    <Gutter>
+      <h1 className={classes.title}> Welcome, [firstName]! </h1>
 
-            <div className={classes.eventContainer}>
-                <h2> Events Information </h2>
-                <p className={classes.eventInfoText}> You must sign up for an event to volunteer for it. </p>
-                
-                <CardContainer>
-                    <Card title="Event 1"> </Card>
-                    <Card title="Event 2"> </Card>
-                    <Card title="Event 3"> </Card>
-                    <Card title="Event 4"> </Card>
-                    <Card title="Event 5"> </Card>
-                    <Card title="Event 6"> </Card>
-                </CardContainer>
-            </div>
-        {/* </Gutter> */}
-        </div>
-    </div>
+      <div className={classes.grayContainer}>Account Type: Volunteer</div>
+
+      <div className={classes.eventContainer}>
+        <h2> Events Information </h2>
+        <p className={classes.eventInfoText}>You must sign up for an event to volunteer for it.</p>
+
+        {/* placeholder cards */}
+        <CardContainer>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 1"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 2"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 3"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 4"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 5"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+          <Card
+            className="test"
+            showCategories={false}
+            hideImagesOnMobile={true}
+            title="Event 6"
+            relationTo="projects"
+            orientation="vertical"
+          ></Card>
+        </CardContainer>
+      </div>
+    </Gutter>
   )
 }

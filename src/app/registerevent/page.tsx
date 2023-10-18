0a14dcbb41ed3e5d8a08f94cc5d7627cce63b0b2
lanceu0128 <lanceu0128@gmail.com>
2023-10-18 18:43:51 -0400

    Slider removed and tailwind styling

--- src/app/registerevent/page.tsx
+++ src/app/registerevent/page.tsx
@@ -5,8 +5,8 @@ export default function RegisterEvent() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Navbar />
-        <h1>Register Event</h1>
+        {/* <Navbar /> */}
+        {/* <h1>Register Event</h1> */}
         <BasicCard
           eventName="being the goat"
           eventDate="everyday"
@@ -14,7 +14,6 @@ export default function RegisterEvent() {
           eventLocation="the farm"
           volunteerRole="the goat"
           volunteerShifts="24/7 365"
-          isMVP={true}
           eventActive={true}
         />
       </div>

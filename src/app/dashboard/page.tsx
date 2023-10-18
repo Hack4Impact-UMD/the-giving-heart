a7d7af96528dc752874c5fcbf13eb69abbcc3117
alston <alston.chan22@sitechhs.com>
2023-10-18 18:15:46 -0400

    redo of Navbar removal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,6 @@ export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Navbar />
         <h1>Dashboard</h1>
         <h1
         // className={classes.title}
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,8 @@
-import SignInPage from "./signIn";
-
 export default function SignIn() {
   return (
     <div>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <SignInPage />
         </div>
       </main>
     </div>

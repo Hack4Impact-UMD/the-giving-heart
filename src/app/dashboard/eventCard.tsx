fc1e904501f12acb6bd632ad254540a890b83a21
Gelila Kebede <gelilakebede123@gmail.com>
2023-10-21 16:08:52 -0400

    improve stlying of dashboard

--- src/app/dashboard/eventCard.tsx
+++ src/app/dashboard/eventCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "../../components/ui/card";
-import { Button, buttonVariants } from "../../components/ui/button";
+import { Card, CardHeader, CardFooter, CardTitle, CardContent } from "../../components/ui/card";
+import { Button } from "../../components/ui/button";
 
 interface EventCardProps {
   title: string;
@@ -8,21 +8,30 @@ interface EventCardProps {
   roles: string;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ title, description, roles }) => {
+export const EventCard: React.FC<EventCardProps> = ({ title, description, roles }) => {
+  const rolesList = roles.split(',').map(role => role.trim());
+
   return (
-    <div className="shadow-md max-w-md rounded-none">
-      <Card className="h-full flex flex-col justify-between">
+    <div className="drop-shadow-[0_10px_10px_rgba(0,0,0,0.50)]"> 
+      <Card className="border-none h-full flex flex-col justify-between rounded-none">
         <CardHeader className="bg-neutral-400 text-black text-center py-4">
           <CardTitle className="py-1"> {title} </CardTitle>
         </CardHeader>
         
-        <CardContent className="rounded-none flex flex-col justify-center items-center flex-1">
-          <div className="py-1 text-center mb-2 text-gray-700"> {description} </div>
-          <div className="py-1 text-gray-700">{roles}</div>
+        <CardContent className="rounded-none flex flex-col justify-start items-start flex-1 p-4">
+          <div className="pl-2 mb-2 text-center italic text-gray-700"> [Description & location] {description} </div>
+          <div className="pl-2 text-gray-700 italic">
+            Roles:
+            <ul className="list-disc pl-8">
+                {rolesList.map((role, index) => (
+                    <li key={index}>{role}</li>
+                ))}
+            </ul>
+          </div>
         </CardContent>
 
-        <CardFooter className="flex justify-center items-center rounded-none">
-          <Button variant="default" size="default" className="bg-neutral-500 text-white rounded-[0.7rem]">
+        <CardFooter className="flex justify-center items-center">
+          <Button variant="default" size="default" className="bg-neutral-400 text-white rounded-[0.7rem]">
             Signup
           </Button>
         </CardFooter>
@@ -30,5 +39,3 @@ const EventCard: React.FC<EventCardProps> = ({ title, description, roles }) => {
     </div>
   );
 };
-
-export default EventCard;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,46 +1,63 @@
-import EventCard from "./eventCard";
+import { EventCard } from "./eventCard";
+
+//placeholder event data. replace with fetched data
+const eventData = [
+  {
+    title: "Event 1",
+    description: "Description for Event 1. ",
+    roles: "Role 1, Role 2, Role 3",
+  },
+  {
+    title: "Event 2",
+    description: "Description for Event 2.",
+    roles: "Role 1",
+  },
+  {
+    title: "Event 3",
+    description: "Description for Event 3.",
+    roles: "Role 1, Role 2",
+  },
+  {
+    title: "Event 4",
+    description: "Description for Event 4",
+    roles: "Role 1, Role 2, Role 3, Role 4, Role 5",
+  },
+  {
+    title: "Event 5",
+    description: "Description for Event 5.",
+    roles: "Role 1, Role 2",
+  },
+  {
+    title: "Event 6",
+    description: "Description for Event 6.",
+    roles: "Role 1, Role 2, Role 3",
+  },
+];
 
 export default function Dashboard() {
   return (
-    <main className="flex min-h-screen flex-col justify-center p-24">
+    <main className="flex min-h-screen flex-col justify-center p-16">
       <div className="text-left mb-4">
-        <h1 className="font-semibold text-3xl mb-4">Welcome, [firstName]!</h1>
-        <div className="bg-neutral-400 text-white py-2 px-4 md:rounded-md rounded-full inline-block mb-4">Account Type: Volunteer</div>
-        <h2 className="font-semibold text-xl mb-2">Events Information</h2>
-        <p className="text-red-500 italic font-bold mb-4">You must sign up for an event to volunteer for it.</p>
+        <h1 className="font-semibold text-5xl mb-4"> Welcome, [firstName]! </h1>
+        <div className="bg-neutral-400 text-white py-2 px-4 md:rounded-md rounded-full inline-block mb-4 min-w-min"> Account Type: Volunteer </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-10">
-        <EventCard
-          title="Event 1"
-          description="Description for Event 1. This is a longer description to test the responsiveness of the card."
-          roles="Role 1, Role 2"
-        />
-        <EventCard
-          title="Event 2"
-          description="Description for Event 2."
-          roles="Role 3"
-        />
-        <EventCard
-          title="Event 3"
-          description="Description for Event 3."
-          roles="Role 4, Role 5"
-        />
-        <EventCard
-          title="Event 4"
-          description="Description for Event 1. This is a longer description to test the responsiveness of the card."
-          roles="Role 1, Role 2"
-        />
-        <EventCard
-          title="Event 5"
-          description="Description for Event 2."
-          roles="Role 3"
-        />
-        <EventCard
-          title="Event 6"
-          description="Description for Event 3."
-          roles="Role 4, Role 5"
-        />
+      <div className="lg:m-10">
+        <div className="text-left">
+          <h2 className="font-semibold text-3xl mb-2"> Events Information </h2>
+          <p className="text-red-500 italic text-lg font-bold mb-4"> You must sign up for an event to volunteer for it. </p>
+        </div>
+
+        <div className="w-full self-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-10">
+          {eventData.map((event, index) => (
+              <EventCard
+                key={index}
+                title={event.title}
+                description={event.description}
+                roles={event.roles}
+              />
+          ))}
+        </div>
       </div>
     </main>
   );

ffca7b61187f59d0916e7c9648e8531f41cabea2
Juan <jmanuelventura46@gmail.com>
2023-10-22 11:07:14 -0400

    saving to test push

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -3,11 +3,12 @@ import Navbar from "../navbar";
 export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between">
         <h2>Background Image</h2>
         <h1>Get Involved</h1>
         <h2>Background Image</h2>
       </div>
+      <div className=""></div>
     </main>
   );
 }

2696225d953b00769589fccf40179878f4fab805
Juan <jmanuelventura46@gmail.com>
2023-10-22 10:00:08 -0400

    for testing purposes

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -4,7 +4,9 @@ export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h2>Background Image</h2>
         <h1>Get Involved</h1>
+        <h2>Background Image</h2>
       </div>
     </main>
   );

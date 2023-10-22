a7c9408b6e03b2f4efc50cfbea848344bfb7c56b
alston <alston.chan22@sitechhs.com>
2023-10-22 01:39:32 -0400

    sign in, sign up, and forgot password pages
    Redid the following pages with Shadcn. Form is ready to be connected to DB, currently just console logging.

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,115 @@
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function SignIn() {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (values: FormData) => {
+    console.log(values);
+  };
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+  
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <main className="flex min-h-screen flex-col items-center justify-between p-16">
+        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+            <div className="flex flex-col w-full md:flex-row mx-auto">
+              <div className="bg-gray-300 p-6 flex flex-col justify-between items-center md:w-full">
+                <div className="flex flex-col items-center">
+                  <h1
+                    className="text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl"
+                  >
+                    Lorem ips
+                  </h1>
+
+                  <p className="text-center italic my-4 text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam
+                    dolorum aliquam, quibusdam aperiam voluptatum.
+                  </p>
+                </div>
+
+                <div className="h-60 w-full bg-white">
+                  <p>Logo goes here</p>
+                </div>
+                
+              </div>
+
+              <div className="flex flex-col bg-white p-6 md:w-full">
+                <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">Login</h1>
+                <div className="w-full mt-12">
+                  <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                              <Input placeholder="email@example.com" {...field} />
+                            </FormControl>
+                            {errors.email && <FormMessage>{errors.email.message}</FormMessage>}
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                              <Input type="password" placeholder="Password" {...field} />
+                            </FormControl>
+                            {errors.password && <FormMessage>{errors.password.message}</FormMessage>}
+                            <FormDescription>
+                              <a href="/forgot-password" className="text-red-500 text-xs hover:underline">Forgot password?</a>
+                            </FormDescription>
+                          </FormItem>
+                        )}
+                      />
+
+                      <div className="flex flex-col justify-center items-center pt-8">
+                        <Button type="submit" className="bg-gray-400 text-white px-12">Login</Button>
+                        <div className="mt-2 text-center text-xs">
+                          <span>Need to make an account? </span>
+                          <a href="/sign-up" className="text-red-500 hover:underline">Sign Up</a>
+                        </div>
+                      </div>
+                      
+                    </form>
+                  </Form>
+                </div>
+            </div>
+        </div>
         </div>
       </main>
     </div>

5413f6836f57dd0a2d3a18c8396d62d39052ea2e
Neha <neharvp@gmail.com>
2023-10-24 19:47:02 -0400

    About Page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,82 +1,97 @@
 import Navbar from "../navbar";
 import Image from "next/image";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 
 export default function About() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>About</h1>
-        <div>
-          <main
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                flexDirection: "row",
-                justifyContent: "center",
-                gap: "10px",
-                backgroundColor: "white",
-              }}
-            >
-              <div className="column">
-                {/* <Image
-                  src="link"
-                  alt="Background Image"
-                  width="100"
-                  height="100"
-                />{" "} */}
-              </div>
-              <div className="column">
-                <h2 style={{ color: "black" }}>Est. 2002</h2>{" "}
-              </div>
-              <div className="column">
-                {/* <Image
-                  src="link"
-                  alt="Background Image"
-                  width="100"
-                  height="100"
-                />{" "} */}
-              </div>
+    <main className="flex min-h-screen flex-col items-center">
+        <div className="flex w-full bg-neutral-400 h-50" >
+
+            <div className="sm:w-1/3 h-full p-4 md:text-center text-left text-white">
+            <div className="mt-12"><h1 className="text-1xl">Background Image</h1></div>
             </div>
 
-            <div>
-              <h2 style={{ display: "flex", justifyContent: "center" }}>
-                Who We Are
-              </h2>
-              <p style={{ display: "flex", justifyContent: "center" }}>
-                This is the content for the section.
-              </p>
+            <div className="w-1/3 md:p-16 p-2 pt-16 text-black bold">
+            <h2 className="text-center text-sm md:text-4xl pt-3 pb-3">Est. 2002</h2>
             </div>
 
-            <div>
-              <div>
-                <h2 style={{ display: "flex", justifyContent: "center" }}>
-                  Ways We Help Others
-                </h2>
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    flexDirection: "row",
-                    justifyContent: "center",
-                    gap: "10px",
-                  }}
-                >
-                  {/* <Image src="link" alt=" Image" width="100" height="100" />
-                  <Image src="link" alt=" Image" width="100" height="100" />
-                  <Image src="link" alt=" Image" width="100" height="100" /> */}
-                </div>
-              </div>
+            <div className="sm:w-1/3 h-full p-4 md:text-center text-left text-white">
+            <div className="mt-12"><h1 className="text-1xl">Background Image</h1></div>
             </div>
-          </main>
+
         </div>
-      </div>
-    </main>
+
+        <div>
+            <div className="flex w-full items-center">
+                <div className="h-full p-4 md:text-center text-left text-black">
+                    <div className="mt-12"><h1 className="text-3xl">Who We Are</h1></div>
+                    <p className="md:text-lg text-xs">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div className="flex w-full items-center">
+                <div className="h-full p-4 md:text-center text-left text-black">
+                    <div className="mt-12"><h1 className="text-3xl">Ways we help others</h1></div>
+                    <div className="flex justify-around w-11/12 m-auto mt-10">
+                        <div className="lg:w-72 md:w-56 sm:w-40 w-24 h-100 sm:h-60 bg-neutral-300 mx-4"></div>
+                        <div className="lg:w-72 md:w-56 sm:w-40 w-24 h-100 sm:h-60 bg-neutral-300 mx-4"></div>
+                        <div className="lg:w-72 md:w-56 sm:w-40 w-24 h-100 sm:h-60 bg-neutral-300 mx-4"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <div className="flex w-full items-center">
+                <div className="h-full p-4 md:text-center text-left text-black">
+                    <div className="mt-12"><h1 className="text-3xl">Our Timeline</h1></div>
+                </div>
+            </div>
+        </div>
+
+       <div>
+       <Accordion style={{ backgroundColor: '#F5F5F5' }}>
+        <AccordionSummary aria-controls="panel1a-content" id="panel1a-header" style={{ backgroundColor: '#D3D3D3' }}>
+            <Typography>Our Beginning: Early 2000&apos;s</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+        </AccordionDetails>
+        </Accordion>
+
+        <Accordion style={{ backgroundColor: '#F5F5F5' }}>
+        <AccordionSummary aria-controls="panel1a-content" id="panel1a-header" style={{ backgroundColor: '#D3D3D3' }}>
+            <Typography>Growth Phase: 2005 - 2009</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+        </AccordionDetails>
+        </Accordion>
+
+        <Accordion style={{ backgroundColor: '#F5F5F5' }}>
+        <AccordionSummary aria-controls="panel1a-content" id="panel1a-header" style={{ backgroundColor: '#D3D3D3' }}>
+            <Typography>Fighting hunger &amp; our future: 2010 - Beyond</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+            <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </Typography>
+        </AccordionDetails>
+        </Accordion>
+        </div>
+      </main>
   );
 }

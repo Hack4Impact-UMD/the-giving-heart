26613ecedbe9534f5669574be01b0b52070de4f4
alston <alston.chan22@sitechhs.com>
2023-10-29 11:03:58 -0400

    updated settings for high fidelity

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -8,7 +8,6 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -18,15 +17,13 @@ import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Eye, EyeOff } from 'lucide-react';
 
 const schema = z.object({
-  email: z.string().email(),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters",
   }),
@@ -35,21 +32,25 @@ const schema = z.object({
 type FormData = z.infer<typeof schema>;
 
 export default function Settings() {
-  const { formState: { errors } } = useForm<FormData>({
-    resolver: zodResolver(schema),
-  });
-
   const form = useForm<FormData>({
     resolver: zodResolver(schema),
   });
 
+  const { formState: { errors } } = form;
   const [isEditing, setIsEditing] = useState(false);
-  const [email, setEmail] = useState('test@gmail.com');
+  const [email] = useState('test@gmail.com');
   const [password, setPassword] = useState('Password123');
-  
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  useEffect(() => {
+    form.setValue('password', password);
+  }, [password, form]);
 
   const onSubmit = (values: FormData) => {
-    setEmail(values.email);
     setPassword(values.password);
     setIsEditing(false);
   };
@@ -58,87 +59,101 @@ export default function Settings() {
     setIsEditing(true);
   };
 
+  const handleCancel = () => {
+    form.reset({ password });
+    setIsEditing(false);
+  };
+
   const passwordAsterisks = '*'.repeat(password.length);
-  
+
   return (
     <div>
-      <main className="flex min-h-screen flex-col items-center justify-between p-16">
+      <div className="z-0 w-full h-40 bg-red-900">
+         .
+      </div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-16 -mt-44">
         <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
-            <div className="w-full">
-                <h1 className="text-2xl font-bold text-gray-900 mb-8 sm:text-3xl md:text-4xl">Settings</h1>
-                <Card className="w-full">
-                    <CardHeader>
-                        <CardTitle>Contact Information</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        {isEditing ? (
-                            <div className="w-full mb-8">
-                                <Form {...form}>
-                                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                                    <FormField
-                                        control={form.control}
-                                        name="email"
-                                        render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Email</FormLabel>
-                                            <FormControl>
-                                            <Input placeholder="email@example.com" {...field} />
-                                            </FormControl>
-                                            {errors.email && <FormMessage>{errors.email.message}</FormMessage>}
-                                        </FormItem>
-                                        )}
-                                    />
-                                    <FormField
-                                        control={form.control}
-                                        name="password"
-                                        render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Password</FormLabel>
-                                            <FormControl>
-                                            <Input type="password" placeholder="Password" {...field} />
-                                            </FormControl>
-                                            {errors.password && <FormMessage>{errors.password.message}</FormMessage>}
-                                        </FormItem>
-                                        )}
-                                    />
-
-                                    <div className="flex flex-col">
-                                        <Button type="submit" className="bg-gray-400 text-white px-12 w-12">Save</Button>
-                                    </div>
-                                    
-                                    </form>
-                                </Form>
-                            </div>
-                        ) : (
-                            <div className="flex flex-col text-lg mb-6">
-                                <div>
-                                    <p className="font-bold">Email:</p>
-                                    <p className="text-gray-600">{email}</p>
+          <div className="w-full">
+            <h1 className="text-3xl font-semibold text-white mb-8 sm:text-3xl md:text-4xl">Settings</h1>
+            <Card className="w-full">
+              <CardHeader>
+                <CardTitle>Contact Information</CardTitle>
+              </CardHeader>
+              <CardContent>
+                {isEditing ? (
+                  <div className="w-full mb-8">
+                    <div className="text-lg">
+                      <p className="font-semibold">Email:</p>
+                      <p className="text-gray-600">{email}</p>
+                    </div>
+                    <Form {...form}>
+                      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 mt-2">
+                        <FormField
+                          control={form.control}
+                          name="password"
+                          render={({ field }) => (
+                            <FormItem>
+                              <FormLabel className="text-lg font-semibold">New Password:</FormLabel>
+                              <FormControl className="relative">
+                                <div className="flex items-center">
+                                  <Input
+                                    type={showPassword ? "text" : "password"}
+                                    placeholder="Password"
+                                    {...field}
+                                    className="pr-10"
+                                  />
+                                  <div
+                                    className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                                    onClick={togglePasswordVisibility}
+                                  >
+                                    {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+                                  </div>
                                 </div>
-                                <div className="mt-2">
-                                    <p className="font-bold">Password:</p>
-                                    <p className="text-gray-600">{passwordAsterisks}</p>
-                                </div>
-                            </div>
-                        )}
-                        <CardTitle>Preferences</CardTitle>
-                        <div className="items-top flex space-x-2 mt-4">
-                            <Checkbox id="terms1" />
-                            <div className="flex flex-row items-center">
-                                <label
-                                htmlFor="terms1"
-                                className="text-sm leading-none text-gray-600 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                                >
-                                I would like to recieve emails from The Giving Heart&apos;s optional newsletter.
-                                </label>
-                            </div>
-                        </div>
-                    </CardContent>
-                </Card>
-                <div className="flex justify-center mt-8">
-                    <Button type="button" onClick={handleEdit} className="bg-gray-400 text-white w-40">Edit Information</Button>
+                              </FormControl>
+                              {errors.password && <FormMessage>{errors.password.message}</FormMessage>}
+                            </FormItem>
+                          )}
+                        />
+                      </form>
+                    </Form>
+                  </div>
+                ) : (
+                  <div className="flex flex-col text-lg mb-6">
+                    <div>
+                      <p className="font-semibold">Email:</p>
+                      <p className="text-gray-600">{email}</p>
+                    </div>
+                    <div className="mt-2">
+                      <p className="font-semibold">Password:</p>
+                      <p className="text-gray-600">{passwordAsterisks}</p>
+                    </div>
+                  </div>
+                )}
+                <CardTitle>Preferences</CardTitle>
+                <div className="items-top flex space-x-2 mt-4">
+                  <Checkbox id="terms1" />
+                  <div className="flex flex-row items-center">
+                    <label
+                      htmlFor="terms1"
+                      className="text-sm leading-none text-gray-600 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      I would like to receive emails from The Giving Heart's optional newsletter.
+                    </label>
+                  </div>
                 </div>
-            </div>
+                <div className="flex justify-center mt-8 space-x-2">
+                  {isEditing ? (
+                    <div className="flex flex-row space-x-2">
+                      <Button type="button" onClick={handleCancel} className="bg-red-600 text-white px-12 w-12">Cancel</Button>
+                      <Button type="submit" className="bg-red-500 text-white px-12 w-12">Save</Button>
+                    </div>
+                  ) : (
+                    <Button type="button" onClick={handleEdit} className="bg-red-600 text-white w-40">Edit Information</Button>
+                  )}
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </main>
     </div>

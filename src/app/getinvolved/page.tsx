66af6ecef56e67eea3a1ac87875c46aa4ff3dd86
Juan <jmanuelventura46@gmail.com>
2023-11-01 18:01:44 -0400

    image

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -5,7 +5,7 @@ export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-h-screen">
       <div className="bg-image-bg bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between"
-      style={{backgroundImage: `url('src/app/getinvolved/getinv.png')`}}>
+      style={{backgroundImage: `url('./getinv.png')`}}>
         <h2>Background Image</h2>
         <h1 className='text-4xl font-sans font-bold dark:text-white'>Get Involved</h1>
         <h2>Background Image</h2>

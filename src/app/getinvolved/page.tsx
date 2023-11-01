b37fe0e3d7f74907f091a5679bd0b5a9997dabaf
Juan <jmanuelventura46@gmail.com>
2023-11-01 17:50:26 -0400

    adding image bg

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "../navbar";
 export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-h-screen">
-      <div className="bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between">
+      <div className="bg-image-bg bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between">
         <h2>Background Image</h2>
         <h1 className='text-4xl font-sans font-bold dark:text-white'>Get Involved</h1>
         <h2>Background Image</h2>

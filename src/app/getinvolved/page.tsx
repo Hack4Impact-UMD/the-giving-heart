c112d780738a0220c12fb4a43b3560faf8157f37
Juan <jmanuelventura46@gmail.com>
2023-11-01 17:58:50 -0400

    image fix

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -1,9 +1,11 @@
+import { url } from "inspector";
 import Navbar from "../navbar";
 
 export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-h-screen">
-      <div className="bg-image-bg bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between">
+      <div className="bg-image-bg bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between"
+      style={{backgroundImage: `url('src/app/getinvolved/getinv.png')`}}>
         <h2>Background Image</h2>
         <h1 className='text-4xl font-sans font-bold dark:text-white'>Get Involved</h1>
         <h2>Background Image</h2>

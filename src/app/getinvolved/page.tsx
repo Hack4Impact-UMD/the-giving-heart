59ebf3371cbd26d2f9be76e2c86f700b82e2df56
Juan <jmanuelventura46@gmail.com>
2023-11-01 17:21:18 -0400

    update fonts

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -5,13 +5,19 @@ export default function GetInvolved() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 min-h-screen">
       <div className="bg-colors-background z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex justify-between">
         <h2>Background Image</h2>
-        <h1 className='text-4xl font-bold dark:text-white'>Get Involved</h1>
+        <h1 className='text-4xl font-sans font-bold dark:text-white'>Get Involved</h1>
         <h2>Background Image</h2>
       </div>
+
       <div className="flex flex-col">
         <div className='flex flex-col'>
-          <h1 className='text-4xl font-bold dark:text-white'>Volunteer Roles</h1>
+          <h1 className='text-4xl font-bold font-sans dark:text-white'>Volunteer Roles</h1>
         </div>
+      </div>
+
+      <div className='flex flex-col'>
+        <h1 className='text-4xl font-bold font-sans dark:text-white'>Donate</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br></br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris<br></br> nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </main>
   );

68609a04e794bd8b1f7b999a7be5f0b84d52c12c
Juan <jmanuelventura46@gmail.com>
2023-11-01 17:28:38 -0400

    formatting fixes

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -15,9 +15,9 @@ export default function GetInvolved() {
         </div>
       </div>
 
-      <div className='flex flex-col'>
+      <div className='flex flex-col items-center'>
         <h1 className='text-4xl font-bold font-sans dark:text-white'>Donate</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br></br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris<br></br> nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className='font-sans dark:text-white'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br></br>eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris<br></br> nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </main>
   );

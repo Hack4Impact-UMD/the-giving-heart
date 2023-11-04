c0f27ffd6438758a13ce4cc8fd9dd8a40de5b7cb
Gelila Kebede <gelilakebede123@gmail.com>
2023-11-03 23:22:23 -0400

    dashboard info section + styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { EventCard } from "./eventCard";
+import Image from "../../../node_modules/next/image";
+import warning_icon from ".././_images/warning.svg";
 import header_image from ".././_images/header-image.jpg";
 
 const eventData = [
@@ -60,33 +62,37 @@ const eventData = [
 
 export default function Dashboard() {
   return (
-    <main className="relative bg-[#860E13] flex min-h-screen flex-col justify-center p-16">
-      <h1 className="font-medium font-openSans text-5xl mb-4 text-white flex items-center justify-center"> Welcome! </h1>
+    <div className="bg-[#860E13] pt-16">
+      <h1 className="pb-4 font-medium font-openSans text-5xl mb-4 text-white flex items-center justify-center"> Welcome! </h1>
 
-      <div className="bg-white w-full flex flex-col items-center justify-center p-4 mt-6 mb-10">
-        <h2 className="font-semibold text-3xl mb-2 pt-4"> Upcoming Events Information </h2>
-        <p className="text-[#9D5425] italic text-lg mb-4"> <strong> Note:</strong> You must register for an event to volunteer for it. </p>
+      <div className="drop-shadow-[0_10px_10px_rgba(0,0,0,0.30)] bg-white w-full flex flex-col items-center justify-center p-6 mt-6 mb-10">
+        <h2 className="font-semibold text-3xl mb-2 text-center"> Upcoming Events Information </h2>
+        <div className="flex flex-row items-center justify-center m-4">
+          <Image src={warning_icon} alt="description icon" width={40} height={40} className="mr-2" />
+          <p className="text-[#9D5425] self-center italic text-lg"> <strong> Note:</strong> You must register for an event to volunteer for it. </p>
+        </div>
         <div className="border-2 border-[#838383] text-[#838383] py-2 px-4 rounded-full inline-block mb-4 min-w-min"> Account Type: Volunteer </div>
       </div>
 
-
-      <div className="lg:m-10">
-        <div className="w-full self-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-10">
-          {eventData.map((event, index) => (
-              <EventCard
-                key={index}
-                image={header_image}
-                title={event.title}
-                description={event.description}
-                roles={event.roles}
-                date={event.date}
-                start_time={event.start_time}
-                end_time={event.end_time}
-                location={event.location}
-              />
-          ))}
+      <main className="flex min-h-screen flex-col justify-center px-16">
+        <div className="lg:m-10">
+          <div className="w-full self-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-10">
+            {eventData.map((event, index) => (
+                <EventCard
+                  key={index}
+                  image={header_image}
+                  title={event.title}
+                  description={event.description}
+                  roles={event.roles}
+                  date={event.date}
+                  start_time={event.start_time}
+                  end_time={event.end_time}
+                  location={event.location}
+                />
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }

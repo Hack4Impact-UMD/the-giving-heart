c50b478247341bc433162f3933b77f04cd25a98c
lanceu0128 <lanceu0128@gmail.com>
2023-11-05 01:54:18 -0400

    mid-fidelity mockup; done expect for switches/icon

--- src/app/registerevent/registerEvent.tsx
+++ src/app/registerevent/registerEvent.tsx
@@ -2,119 +2,213 @@
 
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
+import Switch from '@mui/material/Switch';
 import CardContent from "@mui/material/CardContent";
+import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 // required props; some could be optional but not sure which
 interface EventData {
   eventName: string;
+  eventDescription: string;
+  eventImage: string;
   eventDate: string;
-  eventTime: string;
+  eventStartTime: string;
+  eventEndTime: string;
   eventLocation: string;
   volunteerRole: string;
   volunteerShifts: string; // fields to be filled in by database
   eventActive: boolean;    // used to switch between active and inactive states
 }
 
+// Create a custom Material-UI theme with Open Sans font
+const theme = createTheme({
+  typography: {
+    fontFamily: "Open Sans, sans-serif", // Use Open Sans font
+  },
+});
+
 export default function BasicCard(props: EventData) {
   return (
-    <Card
-      sx={{ minWidth: 350, maxWidth: "65%", display: "flex"}}
-      className="border border-black" // mixed tailwind and material UI styles; component was made before codebase switch
-    >
-      <CardContent sx={{ mb: 5 }}>
-        {/* Title */}
-        <Typography
-          sx={{ color: "red", fontSize: 28, mt: 2 }}
-          color="text.secondary"
-          gutterBottom
-        >
-          <b>{props.eventName}</b>
-        </Typography>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Card
+      sx={{ width: 425}}
+      className="px-0 flex flex-col" // mixed tailwind and material UI styles; component was made before codebase switch
+      >    
+        <CardContent sx={{ mb: 1, px: 3}}> 
+          {/* Title */}
+          <Typography
+            sx={{color: "#860E13", fontSize: 28, mt: 1, mb: 1}}
+            color="text.secondary"
+            gutterBottom
+            >
+            <b>{props.eventName}</b>
+          </Typography>
+          
+          <Typography
+            sx={{color: 'black', fontSize: 20}}
+            className="font-semibold"
+            gutterBottom
+            >
+            Event Information
+          </Typography>
 
-        {/* Main Content */}
-        <Typography variant="body2">
-          <b>Date:</b> {props.eventDate} - {props.eventTime}
-          <br />
-          <b>Location:</b> {props.eventLocation}
-          <br />
-          <b>Volunteer Role:</b> {props.volunteerRole}
-          <br />
-          <b>Shifts:</b> {props.volunteerShifts}
-          <br />
-        </Typography>
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/book-open.svg" alt="book-icon"/>
 
-        {/* Card Actions */}
-        <CardContent
-          sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", gap: "20px", p: 0 }}
-          className="md:flex-row"
-        >
-          {/* left/top side actions */}
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              flex: 1,
-              p: 0,
-              mt: 3,
-              color: "red",
-            }}
-          >
-            <Typography variant="body1">
-              <b>
-                If you would like to drop your registration for this event,
-                please click the button below.
-              </b>
-            </Typography>
-            <Box sx={{ mt: 1 }}>
-              {/* ADD ONCLICK BEHAVIOR HERE */}
-              <button className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md">
-                Check In
-              </button>
-            </Box>
-          </Box>
-          {/* right/bottom side actions */}
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              flex: 1,
-              p: 0,
-              mt: 3,
-              color: "red",
-            }}
-          >
-            <Typography variant="body1">
-              <b>
-                When the event is active, please check in/out of the event using these buttons.
-              </b>
-            </Typography>
-
-            <Box sx={{ display: "flex", mt: 1 }}>
-              {/* ADD CHECK IN ONCLICK BEHAVIOR */}
-              <button
-                className={`${
-                  props.eventActive ? 'bg-green-600 hover:bg-green-700' : 'bg-gray-500 hover:bg-gray-600 shadow-none'
-                } text-white px-4 py-2 mr-4 rounded-md`} // change color based on active event
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: 'black', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
               >
-                Check In
-              </button>
-              {/* ADD CHECK OUT ONCLICK BEHAVIOR */}
-              <button
-                className={`${
-                  props.eventActive ? 'bg-orange-500 hover:bg-orange-600' : 'bg-gray-500 hover:bg-gray-600 shadow-none'
-                } text-white px-4 py-2 rounded-md`} // change color based on active event
+                <b>Description: </b>
+              </Typography>
+              <Typography
+                sx={{ color: 'gray  ', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+                >
+                {props.eventDescription}
+              </Typography>
+            </div>
+          </div>
+
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/calendar-clock.svg" alt="calendar-clock-icon"/>
+
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: 'black', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+                >
+                <b>Date & Time: </b>
+              </Typography>
+              <Typography
+                sx={{ color: 'gray  ', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+                >
+                Date: {props.eventDate} from {props.eventStartTime} - {props.eventEndTime}
+              </Typography>
+            </div>
+          </div>
+
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/globe-2.svg" alt="globe-2-icon"/>
+
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: 'black', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+                >
+                <b>Location</b>
+              </Typography>
+              <Typography
+                sx={{ color: 'gray  ', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+                >
+                {props.eventLocation}
+              </Typography>
+            </div>
+
+            <hr className="border-t border-gray-300 my-4" />
+          </div>
+
+          <Typography
+            sx={{color: 'black', fontSize: 20, mt: 1}}
+            color="text.secondary"
+            gutterBottom
+            >
+            <b>Role Information</b>
+          </Typography>
+
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/users.svg" alt="users-icon"/>
+
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: 'black', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
               >
-                Check Out
-              </button>
-            </Box>
-          </Box>
+                <b>Volunteer Role: </b>
+              </Typography>
+              <Typography
+                sx={{ color: 'gray  ', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                {props.volunteerRole}
+              </Typography>
+            </div>
+          </div>
+
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/trash-2.svg" alt="trash-2-icon"/>
+
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: 'black', fontSize: 16 }}
+                color="text.secondary"
+                gutterBottom
+              >
+                <b>Drop event registration: </b>
+              </Typography>
+            </div>
+          </div>
+
+          <div className="text-center">
+            <button className="bg-red-500 hover:bg-red-600 text-white px-10 py-2 rounded-md mt-3 inline-block">
+              <b>Drop Spot</b>
+            </button>
+          </div>
+
         </CardContent>
-      </CardContent>
-    </Card>
+        <Box sx={{backgroundColor: "#860E13", px: 3, py: 3}}>
+          <Typography
+            sx={{color: 'white', fontSize: 20 }}
+            >
+            <b>Event Check-In/Out</b>
+          </Typography>
+
+          <div className="flex space-x-2 items-start mt-3">
+            <img src="/_images/users.svg" alt="users-icon"/>
+
+            <div className="flex flex-col">
+              <Typography
+                sx={{ color: '#E6E5E5', fontSize: 14 }}                >
+                <i>When the event has started, please check in/out of the event by clicking the switches below.</i>
+              </Typography>
+            </div>
+          </div>
+
+          <div className="flex flex-row text-center justify-evenly my-3">
+            <div className="flex flex-col items-center">
+              <Typography
+                sx={{color: 'white', fontSize: 18 }}
+                >
+                <b>Check-In</b>
+              </Typography>
+              <Switch />
+            </div>
+            <div className="flex flex-col items-center">
+              <Typography
+                sx={{color: 'white', fontSize: 18 }}
+                >
+                <b>Check-Out</b>
+              </Typography>
+              <Switch />
+            </div>
+          </div>
+
+        </Box>
+      </Card>
+    </ThemeProvider>
   );
 }

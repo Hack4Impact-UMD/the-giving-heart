2dcaabc31832072a80faddda214ed793869d8567
thegivingheart-dev <146566132+thegivingheart-dev@users.noreply.github.com>
2023-11-12 11:15:58 -0500

    navbar updates (#26)
    * navbar auth updates
    
    * quick navbar fix
    
    ---------
    
    Co-authored-by: Alex Chen <achen131@umd.edu>

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,24 +4,23 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion"
-
+} from "@/components/ui/accordion";
 
 import { FormItem } from "@/components/ui/form";
 import { useAuthContext } from "@/utils/context/AuthContext";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
 import Image from "../../node_modules/next/image";
-import logo from "./_images/giving-heart-logo.jpeg"
+import logo from "./_images/giving-heart-logo.jpeg";
 
-import { Home } from 'lucide-react';
-import { HelpCircle } from 'lucide-react';
-import { HeartHandshake } from 'lucide-react';
-import { UserCircle2 } from 'lucide-react';
-import { Settings } from 'lucide-react';
-import { CalendarDays } from 'lucide-react';
-import { XCircle } from 'lucide-react';
-import { X } from 'lucide-react';
+import { Home } from "lucide-react";
+import { HelpCircle } from "lucide-react";
+import { HeartHandshake } from "lucide-react";
+import { UserCircle2 } from "lucide-react";
+import { Settings } from "lucide-react";
+import { CalendarDays } from "lucide-react";
+import { XCircle } from "lucide-react";
+import { X } from "lucide-react";
 
 import { removeToken } from "@/utils/helpers";
 
@@ -32,12 +31,10 @@ const navigation = [
 ];
 
 const mobileLinks = [
-  { name: "Home", href: "/"},
+  { name: "Home", href: "/" },
   { name: "About Us", href: "/about" },
   { name: "Get Involved", href: "/dashboard" },
 ];
-
-
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -48,7 +45,12 @@ export default function Navbar() {
   const [isOpen, onClose] = useState(false);
   const { user, setUser } = useAuthContext();
   const [isSlide, OnSlide] = useState(false);
-  const mobileIcons = [<Home key="home" className="m-auto text-[#ed1c24]" />, <HelpCircle key="helpCircle" className="m-auto text-[#ed1c24]" />, <HeartHandshake key="heartHandshake" className="m-auto text-[#ed1c24]" />];
+  const mobileIcons = [
+    <Home key="home" className="m-auto text-[#ed1c24]" />,
+    <HelpCircle key="helpCircle" className="m-auto text-[#ed1c24]" />,
+    <HeartHandshake key="heartHandshake" className="m-auto text-[#ed1c24]" />,
+    <UserCircle2 key="userCircle2" className="m-auto text-[#ed1c24]" />,
+  ];
   const toggleMenu = () => {
     onClose(!isOpen);
     OnSlide(!isSlide);
@@ -64,12 +66,7 @@ export default function Navbar() {
       <div className="-my-px flex space-x-8 bg-white w-full h-20 justify-between">
         <div className="mt-auto mb-auto ml-6">
           <a href="/">
-            <Image
-              src={logo}
-              width={70}
-              height={70}
-              alt="Giving heart logo"
-            />
+            <Image src={logo} width={70} height={70} alt="Giving heart logo" />
           </a>
         </div>
         <div className="hidden md:block m-auto">
@@ -131,66 +128,99 @@ export default function Navbar() {
         className={`fixed inset-0 bg-gray-800 bg-opacity-75 z-40 ${
           isOpen ? "block" : "hidden"
         }`}
+      ></div>
+      <div
+        className={`fixed inset-y-0 right-0 max-w-xs w-full bg-white z-40 shadow-lg p-6 transform duration-300 ease-in ${
+          isSlide ? "translate-x-0" : "translate-x-full"
+        }`}
       >
-      </div>
-        <div
-          className={`fixed inset-y-0 right-0 max-w-xs w-full bg-white z-40 shadow-lg p-6 transform duration-300 ease-in ${
-            isSlide? 'translate-x-0' : 'translate-x-full'
-          }`}
-        >
-          <div className="mb-10 w-full flex flex-row-reverse">
-            <X
-              className="hover:cursor-pointer text-gray-400"
-              onClick={toggleMenu}
-            />
-          </div>
-          <div className="flex-col h-1/3 justify-between ">
-            {mobileLinks.map((item, index) => (
-              <div key={item.name} className="flex">
-                {mobileIcons[index]}
-                <a
-                  
-                  href={item.href}
-                  className={classNames(
-                    pathname === item.href
-                      ? " text-gray-500 hover:text-[#ed1c24]"
-                      : " text-gray-500 hover:text-[#ed1c24]",
-                    "inline-flex w-full p-3"
-                  )}
-                  aria-current={pathname === item.href ? "page" : undefined}
-                >
-                  {item.name}
-                </a>
-             </div>
-
-            ))}
+        <div className="mb-10 w-full flex flex-row-reverse">
+          <X
+            className="hover:cursor-pointer text-gray-400"
+            onClick={toggleMenu}
+          />
+        </div>
+        <div className="flex-col h-1/3 justify-between ">
+          {mobileLinks.map((item, index) => (
+            <div key={item.name} className="flex">
+              {mobileIcons[index]}
+              <a
+                href={item.href}
+                className={classNames(
+                  pathname === item.href
+                    ? " text-gray-500 hover:text-[#ed1c24]"
+                    : " text-gray-500 hover:text-[#ed1c24]",
+                  "inline-flex w-full p-3"
+                )}
+                aria-current={pathname === item.href ? "page" : undefined}
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
+          {user ? (
             <div className="flex">
-              <UserCircle2 className="text-[#ed1c24] mx-auto mt-4"/>  
+              <UserCircle2 className="text-[#ed1c24] mx-auto mt-4" />
               <Accordion type="single" collapsible className="w-full px-3">
                 <AccordionItem value="item-1">
-                
-                  <AccordionTrigger className="text-gray-500">[First Name]&apos;s Profile</AccordionTrigger>
+                  <AccordionTrigger className="text-gray-500">
+                    {user.firstName}&apos;s Profile
+                  </AccordionTrigger>
                   <AccordionContent className="text-gray-500 text-md">
                     <div className="flex">
-                      <Settings className="my-auto mr-2 text-[#ed1c24]"/> <a className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer">Settings</a>
-                    </div>
-                    
-                  </AccordionContent >
-                  <AccordionContent className="text-gray-500 text-md">
-                    <div className="flex">
-                      <CalendarDays className="my-auto mr-2 text-[#ed1c24]"/> <a className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer">Event Management</a>
+                      <Settings className="my-auto mr-2 text-[#ed1c24]" />{" "}
+                      <a
+                        className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer"
+                        href="/settings"
+                      >
+                        Settings
+                      </a>
                     </div>
                   </AccordionContent>
                   <AccordionContent className="text-gray-500 text-md">
                     <div className="flex">
-                      <XCircle className="my-auto mr-2 text-[#ed1c24]"/> <a className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer">Logout</a>
+                      <CalendarDays className="my-auto mr-2 text-[#ed1c24]" />{" "}
+                      <a className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer">
+                        Event Management
+                      </a>
+                    </div>
+                  </AccordionContent>
+                  <AccordionContent className="text-gray-500 text-md">
+                    <div className="flex">
+                      <XCircle className="my-auto mr-2 text-[#ed1c24]" />{" "}
+                      <a
+                        className="text-gray-500 hover:text-[#ed1c24] hover:cursor-pointer"
+                        onClick={() => {
+                          toggleMenu();
+                          handleLogout();
+                        }}
+                      >
+                        Logout
+                      </a>
                     </div>
                   </AccordionContent>
                 </AccordionItem>
               </Accordion>
-              </div>
-          </div>
+            </div>
+          ) : (
+            <div key="Sign Up/Log In" className="flex">
+              {mobileIcons[3]}
+              <a
+                href={"/sign-in"}
+                className={classNames(
+                  pathname === "/sign-in"
+                    ? " text-gray-500 hover:text-[#ed1c24]"
+                    : " text-gray-500 hover:text-[#ed1c24]",
+                  "inline-flex w-full p-3"
+                )}
+                aria-current={pathname === "/sign-in" ? "page" : undefined}
+              >
+                Sign Up/Log In
+              </a>
+            </div>
+          )}
         </div>
+      </div>
     </>
   );
 }

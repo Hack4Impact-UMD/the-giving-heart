f21053b477cdd4ab9298ce97772027798d4ce69f
alston <alston.chan22@sitechhs.com>
2023-11-19 10:59:27 -0500

    mapbox integrated

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -1,10 +1,12 @@
-import Navbar from "../navbar";
+"use client";
+import MapboxMap from "@/components/map";
 
 export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-between flex flex-col">
         <h1>Get Involved</h1>
+        <MapboxMap />
       </div>
     </main>
   );

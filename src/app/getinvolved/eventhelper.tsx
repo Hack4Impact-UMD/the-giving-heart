fe7fcb60ce5e843114508a030a6379d9ecaccc19
Juan <jmanuelventura46@gmail.com>
2023-11-28 23:22:11 -0500

    update content style

--- src/app/getinvolved/eventhelper.tsx
+++ src/app/getinvolved/eventhelper.tsx
@@ -26,12 +26,12 @@ export default function EventHelper() {
         <div key="id">
           <a
             href="#"
-            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            className="border-none block max-w-sm p-6 bg-white rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
           >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-sm md:text-2xl font-sans font-bold tracking-tight text-gray-900 dark:text-white">
               {item["attributes"]["title"]}
             </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
+            <p className="font-sans text-gray-700 font-light text-xs dark:text-gray-400">
               {item["attributes"]["description"]}
             </p>
           </a>

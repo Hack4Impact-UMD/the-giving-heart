7bc7a71f6043debd7be1925e9b5e5ca8db118e5b
Juan <78895623+jmv2003@users.noreply.github.com>
2023-12-01 00:54:11 -0500

    Jv getinvolved (#38)
    * for testing purposes
    
    * saving to test push
    
    * testing layout
    
    * update fonts
    
    * formatting fixes
    
    * adding image bg
    
    * image fix
    
    * image
    
    * background image added, need to adjust margins and Strapi cms
    
    * PR
    
    * fixing merge conflicts
    
    * merge fixed plz
    
    * yarn.lock
    
    * update content style
    
    * boxes added where images go for events
    
    * get-involved update
    
    ---------
    
    Co-authored-by: Dan Nguyen <danthedevnguyen@gmail.com>

--- src/app/getinvolved/page.tsx
+++ src/app/getinvolved/page.tsx
@@ -4,43 +4,42 @@ import EventHelper from "./eventhelper";
 
 export default function GetInvolved() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="z-10 max-w-5xl w-full items-center justify-between flex flex-col">
-        <div
-          className="w-screen flex items-center justify-center sm:bg-contain align-middle font-sans"
-          style={{
-            backgroundImage:
-              "url(https://s3-alpha-sig.figma.com/img/3d9f/533e/7b3a91405e9b8b8200346a425beddb5c?Expires=1701043200&Signature=CJhqEZs6cKN1YmeGEo6Qd4RlHSc4A9wfX3Eopvt-7edn~fQfFufoJW4Unb5Dlt6-3Ka62xic1RTUAm7-iTya4LIlhJghFBrrtJNgLXLNwxpFv~oek7HZSHGiO3eFvjcXWAWFiIshB-JXHTOclTwE9bo2FZxY8er7z6m-R3RbflnbsCfY6Cms2zAnVN9Iflvxtpb7lbuBOkEDoZgfru2oX6dPwRTlAHlV1wxtdEnJKLI~TJyv28OX0tl0riGF8rnyH8uk01NZXJlD2ofpB1t7txDipPlWM4unAgBpPe4L9uCkz0wyQO2V7zgDdg99RNMTy8Tm6DKKUNo3O~JSJpkYeQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            // width: '200vh',
-            height: "30vh",
-            position: "relative",
-          }}
-        >
-          <div>
-            <h1 className="text-5xl font-sans font-bold text-white">
-              Get Involved
-            </h1>
+        <div className="relative">
+          <div
+            className="brightness-50 w-screen flex items-center justify-center sm:bg-contain align-middle font-sans"
+            style={{
+              backgroundImage:
+                "url(https://s3-alpha-sig.figma.com/img/3d9f/533e/7b3a91405e9b8b8200346a425beddb5c?Expires=1702252800&Signature=hXrW7GnPSoC1ODX~1LUYVH0vjnxKHPS0~cqmp4tkkN4GAdO60l8CMNiD2LdO~3EB9kqvS40E4APNqWO-GU1CFUkXyX5~7KHqeNEdraCQPY90CzACk~S53EPsE0Z4jsxp9P7pYh4CIQ0dYBDQxY8nmpCt0m9gb-Hl2X7PDLaQwy3L9AmMwA~7xnYGn37bW7pWqUjsF~wASuy2LxDTD9JrahGTmcNd-NGVQGpNUfZYvAZkqFMCpqpCI1n0WN4VwWu5PqUxHa8tdlUIFE5cRzcqplXsd~E9cJU835vIBGOVkBN7gdfv9GmX8iG9l1poDEcpQKcbOiDMQY2SG1FIt4qzrg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4)",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              // width: '200vh',
+              height: "30vh",
+              position: "relative",
+            }}
+          >
           </div>
-        </div>
-
-        <div className="flex flex-col">
-          <div className="flex flex-col">
-            <h1 className="text-4xl font-bold font-sans dark:text-white">
+          <div className="absolute inset-0 flex items-center justify-center">
+              <h1 className="text-3xl md:text-5xl font-sans font-bold text-white">
+                Get Involved
+              </h1>
+          </div>
+        </div> 
+        <div className="w-5/6 border flex flex-col items-center">
+            <h1 className="border-none text-center text-lg md:text-4xl font-bold font-sans dark:text-white">
               Volunteer Roles
             </h1>
-          </div>
+            <div className="border-none items-center">
+              <EventHelper />
+            </div>
         </div>
 
-        <div className="flex flex-col items-center">
-          <div>
-            <EventHelper />
-          </div>
-          <h1 className="text-4xl font-bold font-sans dark:text-white">
+        <div className="w-5/6 flex flex-col items-center">
+          <h1 className="text-lg md:text-4xl font-bold font-sans dark:text-whit">
             Donate
           </h1>
-          <p className="font-sans dark:text-white">
+          <p className="text-sm font-sans dark:text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do{" "}
             <br></br>eiusmod tempor incididunt ut labore et dolore magna aliqua.
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -50,8 +49,8 @@ export default function GetInvolved() {
             proident, sunt in culpa qui officia deserunt mollit anim id est
             laborum.
           </p>
+          <MapboxMap />
         </div>
-        <MapboxMap />
       </div>
     </main>
   );

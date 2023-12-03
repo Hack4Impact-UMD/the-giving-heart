7f6fa21099b860068cde08ea2ff33856f347f6d0
alston <alston.chan22@sitechhs.com>
2023-12-03 07:34:01 -0500

    apostrophe fix

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -106,7 +106,7 @@ export default function Navbar() {
               <DropdownMenu>
                 <DropdownMenuTrigger>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="inline-flex align-center mr-2 lucide lucide-circle-user-round"><path d="M18 20a6 6 0 0 0-12 0"/><circle cx="12" cy="10" r="4"/><circle cx="12" cy="12" r="10"/></svg>
-                {user?.firstName}'s Profile
+                {user?.firstName}&apos;s Profile
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                   <DropdownMenuItem>

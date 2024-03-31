f4366efed169a667b0a248a6b00c82b37ec90a9a
achou1044 <64855502+achou1044@users.noreply.github.com>
2024-03-31 13:50:39 -0400

    Manually Populated Sections

--- src/app/our-sponsors/page.tsx
+++ src/app/our-sponsors/page.tsx
@@ -1,62 +1,32 @@
 "use client";
 
+import Image from "../../../node_modules/next/image";
+
+{/* Images for Sponsors Section */}
 import APRI from "./Sponsors/APRI.png"
 import BH from "./Sponsors/BoarsHead.png"
 import Bombas from "./Sponsors/Bombas.png"
-import CV from "./Sponsors/ClerviewCounseling.png.png"
-import DE from "./Sponsors/DominionEnergy.png.png"
-import EZB from "./Sponsors/EZBox.png.png"
-import KK from "./Sponsors/KrispyKreme.png.png"
-import ML from "./Sponsors/MassieLaw.png.png"
+import CV from "./Sponsors/ClerviewCounseling.png"
+import DE from "./Sponsors/DominionEnergy.png"
+import EZB from "./Sponsors/EZBox.png"
+import KK from "./Sponsors/KrispyKreme.png"
+import ML from "./Sponsors/MassieLaw.png"
 import NAACP from "./Sponsors/NAACP.png"
-import NPHC from "./Sponsors/NatPanHelllenicCouncil.png.png"
+import NPHC from "./Sponsors/NatPanHelllenicCouncil.png"
 import Pepsi from "./Sponsors/Pepsi.png"
-import RVA from "./Sponsors/RichmondVA.png.png"
-import Sysco from "./Sponsors/Sysco.png.png"
-import Threads from "./Sponsors/Threads.png.png"
+import RVA from "./Sponsors/RichmondVA.png"
+import Sysco from "./Sponsors/Sysco.png"
+import Threads from "./Sponsors/Threads.png"
 import Ukrops from "./Sponsors/Ukrops.png"
-import UH from "./Sponsors/UnitedHeathcare.png.png"
-import UO from "./Sponsors/urban-one-logo 1.png.png"
-import Walmart from "./Sponsors/Walmart.png.png"
-import WB from "./Sponsors/WilliamsBakery.png.png"
+import UH from "./Sponsors/UnitedHeathcare.png"
+import UO from "./Sponsors/urban-one-logo 1.png"
+import Walmart from "./Sponsors/Walmart.png"
+import WB from "./Sponsors/WilliamsBakery.png"
 
+{/* Images for Auxillary Supporters Section */}
 import Aramark from "./AuxSupporters/Aramark.png"
 import GR from "./AuxSupporters/GreaterRichmondConventionCenter.png"
-import Market from "./AuxSupporters/TheMarket25.png.png"
-
-const supporters = ["The Honorable Cynthia I. Newbille  (Councilwoman - 7th  District)", "Eda H. Cabaniss Charitable", 
-                    "St. John's United Church of Christ Richmond, Va.", "The Honorable Lamont Bagby\n(Va. State Senate)",
-                    "The Honorable Lamont Bagby\n(Va. State Senate)", "Clovia Lawrence, Margaret Grooms and Family"]
-
-const aux_supporters_text = ["First English Evangelical Lutheran Church","Project Give Back to the Community",  
-                            "Urban League of Richmond Young Professionals"]
-const aux_supporters_imgs = [
-    { image: Aramark, text: "Aramark" },
-    { image: GR, text: "Greater Richmond Convention Center" },
-    { image: Market, text: "The 25th Market" },
-  ]
-
-  const sponsors_imgs = [
-    { image: APRI, text: "APRI" },
-    { image: BH, text: "Boar's Head" },
-    { image: Bombas, text: "Bombas" },
-    { image: CV, text: "Clearview Counseling" },
-    { image: DE, text: "Dominion Energy" },
-    { image: EZB, text: "E-Z Box" },
-    { image: KK, text: "Krispy Kreme" },
-    { image: ML, text: "Massie Law" },
-    { image: NAACP, text: "NAACP" },
-    { image: NPHC, text: "National Pan Hellenic Council" },
-    { image: Pepsi, text: "Pepsi" },
-    { image: RVA, text: "Richmond VA" },
-    { image: Sysco, text: "Sysco" },
-    { image: Threads, text: "Threads" },
-    { image: Ukrops, text: "Ukrops" },
-    { image: UH, text: "United Healthcare" },
-    { image: UO, text: "Urban One" },
-    { image: Walmart, text: "Walmart Stores 1969 & 2821" },
-    { image: WB, text: "William's Bakery" },
-  ]
+import Market from "./AuxSupporters/TheMarket25.png"
 
 export default function OurSponsors() {
   
@@ -97,13 +67,77 @@ export default function OurSponsors() {
         <br/>
         </div>
 
-
         {/* Sponsors Section */}
         <h2 className="sm:pb-8 pb-2 text-xl font-semibold">
             Our Sponsors
         </h2>
-        <div className="grid grid-cols-3 gap-4">
-          
+        <div className="grid grid-cols-3 gap-4 basis-1/3 text-center w-6/7 lg:w-3/4 ml-10 mr-10 mb-10 place-items-center">
+          <Image src={UO} alt="Urban One"/>
+          <Image src={UH} alt="United Healthcare" />
+          <Image src={Sysco} alt="Sysco" />
+          <Image src={DE} alt="Dominion Energy" />
+          <Image src={Threads} alt="Threads" />
+          <Image src={Pepsi} alt="Pepsi" />
+          <Image src={Bombas} alt="Bombas" />
+          <Image src={KK} alt="Krispy Kreme" />
+          <Image src={BH} alt="Boar's Head" />
+          <Image src={EZB} alt="E-Z Box" />
+          <Image src={WB} alt="Williams Bakery" />
+          <Image src={APRI} alt="APRI" />
+          <Image src={NAACP} alt="NAACP" />
+          <Image src={CV} alt="Clearview Counseling" />
+          <Image src={Ukrops} alt="Ukrops" />
+          <Image src={NPHC} alt="National Pan-Hellenic Council" />
+          <Image src={RVA} alt="Richmond, VA" />
+          <div className="flex flex-col justify-center align-center">
+            <Image className="justify-self-center" src={Walmart} alt="Walmart" />
+            <p className="text-xs text-center">Stores 1969 and 2821</p>
+          </div>
+          <Image src={ML} alt="Massie Law" />
+        </div>
+
+        {/* Supporters Section */}
+        <h2 className="sm:pb-8 pb-2 text-xl font-semibold">
+            Supporters
+        </h2>
+        <div className="grid grid-cols-3 gap-4 basis-1/3 text-center w-6/7 lg:w-3/4 ml-10 mr-10 mb-10 place-items-center">
+          <p className="sm:text-lg text-xs">
+            The Honorable Cynthia I. Newbille <br/>(Councilwoman - 7th District)
+          </p>
+          <p className="sm:text-lg text-xs">
+            Eda H. Cabaniss <br/> Charitable
+          </p>
+          <p className="sm:text-lg text-xs">
+            St. John&apos;s United Church of Christ Richmond, VA
+          </p>
+          <p className="sm:text-lg text-xs">
+            The Honorable Lamont Bagby <br/> (VA. State Senate)
+          </p>
+          <p className="sm:text-lg text-xs">
+            The Honorable Lamont Bagby <br/> (VA. State Senate)
+          </p>
+          <p className="sm:text-lg text-xs">
+            Clovia Lawrence, Margaret Grooms and Family
+          </p>
+        </div>
+
+        {/* Auxillary Supporters Section */}
+        <h2 className="sm:pb-8 pb-2 text-xl font-semibold">
+            Auxillary Supporters
+        </h2>
+        <div className="grid grid-cols-3 gap-4 basis-1/3 text-center w-6/7 lg:w-3/4 ml-10 mr-10 mb-10 place-items-center">
+          <p className="sm:text-lg text-xs">
+            First English Evangelical Lutheran Church
+          </p>
+          <p className="sm:text-lg text-xs">
+            Project Give Back to the Community
+          </p>
+          <p className="sm:text-lg text-xs">
+            Urban League of Richmond Young Professionals
+          </p>
+          <Image src={GR} alt="Greater Richmond Convention Center" />
+          <Image src={Market} alt="The 25th Market" />
+          <Image src={Aramark} alt="Aramark" />
         </div>
 
       </main>
